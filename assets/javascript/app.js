4793a21a14473ffe370df649fcf3f536da7a6625
HelioSchneiderr <helioschndr@gmail.com>
2022-05-18 17:46:00 -0300

    v2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ const fetchPokemon = () =>{
     const url = `https://pokeapi.co/api/v2/pokemon/3`;
     
     fetch(url)
-    .then(resposta = resposta.json())
+    .then(resposta => resposta.json())
     .then(pokemon => {
         console.log(pokemon);
     });
